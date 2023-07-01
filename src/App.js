e12324a21975fa89891c13435075d0250b02be75
Isabella <isabellaj.lizarde@gmail.com>
2023-07-01 00:53:20 -0700

    Created search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import logo from "./logo.svg";
+import React from "react";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello from App.js</p>
+      <header className="App-header pt-5">
+        <h1> Dictionary </h1>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="mt-3">Coded with ♥︎ by Isabella Lizarde</footer>
     </div>
   );
 }
