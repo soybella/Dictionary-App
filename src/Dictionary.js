f5a5546d8c76a96438d098a551dc4307d98f3c85
Isabella <isabellaj.lizarde@gmail.com>
2023-07-02 00:03:59 -0700

    Added dictionary api intergration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+
+    let apiKey = "aeb70a31839f16b6t1e99ad4f4bo55e4";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
-    // console.log(event.target.value);
     setKeyword(event.target.value);
   }
 
