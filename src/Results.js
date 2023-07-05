7546eb5529d386170fcd5d27fecd29700f13619a
Isabella <isabellaj.lizarde@gmail.com>
2023-07-05 15:30:08 -0700

    Created phonetics intergration

--- src/Results.js
+++ src/Results.js
@@ -8,7 +8,8 @@ export default function Results(props) {
     return (
       <div className="Results p-3">
         <div className=" mt-3 pt-3 border rounded mb-3">
-          <h2 className="pb-3">{props.results.word}</h2>
+          <h2>{props.results.word}</h2>
+          <h4 className="pb-3">/{props.results.phonetic}/</h4>
           {props.results.meanings.map(function (meaning, index) {
             return (
               <div key={index}>
